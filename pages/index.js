e45ac5a2ebf37cb00951c841e344ab251ca6415f
0xiliyan <0xiliyan@membernft.io>
2022-05-11 00:23:45 +0300

    Gitignore edit

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
