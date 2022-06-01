afb8eb7a954e30fe40b483a8936c02291c441b72
0xiliyan <0xiliyan@membernft.io>
2022-06-01 15:18:43 +0300

    Testing contract deployment

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity >=0.7.0 <0.9.0;
 
 // OpenZeppelin's ERC721 implementation (it's a standard, so let it be)
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
--- pages/api/deploy-contract.js
+++ pages/api/deploy-contract.js
@@ -1,8 +1,6 @@
-import schema from "../../database/schema.js";
-import {connection} from "../../helpers/db";
 
 export default function handler(req, res) {
-    if (req.method === 'POST') {
+    if (req.method === 'GET') {
         return deployContract(req, res);
     }
 }
@@ -15,7 +13,7 @@ const deployContract = async (req, res) => {
     const Web3 = require('web3');
 
     // Compile contract
-    const contractPath = path.resolve(__dirname, 'NFT_IMX_FULL.sol');
+    const contractPath = path.resolve(`${__dirname}../../../../../contracts`, 'NFT.sol');
     const source = fs.readFileSync(contractPath, 'utf8');
 
     const input = {
@@ -34,40 +32,44 @@ const deployContract = async (req, res) => {
         },
     };
 
+
     const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
-    const contractFile = tempFile.contracts['NFT.sol']['NFT'];
+    // const contractFile = tempFile.contracts['NFT.sol']['NFT'];
 
+    let result = tempFile;
 
-    // Initialization
-    const bytecode = contractFile.evm.bytecode.object;
-    const abi = contractFile.abi;
+    //
+    // // Initialization
+    // const bytecode = contractFile.evm.bytecode.object;
+    // const abi = contractFile.abi;
+    //
+    // const privKey = '<private key>';
+    // const address = '<address>';
+    //
+    // const web3 = new Web3("https://ropsten.infura.io/v3/<api>");
+    //
+    // // Deploy contract
+    // const deploy = async() => {
+    //
+    //     console.log('Attempting to deploy from account:', address);
+    //     const incrementer = new web3.eth.Contract(abi);
+    //
+    //     const incrementerTx = incrementer.deploy({
+    //         data: bytecode
+    //         // arguments: [],
+    //     })
+    //     const createTransaction = await web3.eth.accounts.signTransaction({
+    //             from: address,
+    //             data: incrementerTx.encodeABI(),
+    //             gas: 3000000,
+    //         },
+    //         privKey
+    //     )
+    //     const createReceipt = web3.eth.sendSignedTransaction(createTransaction.rawTransaction).then((res) => {
+    //         console.log('Contract deployed at address', res.contractAddress);
+    //     });
 
-    const privKey = '<private key>';
-    const address = '<address>';
+    // };
 
-    const web3 = new Web3("https://ropsten.infura.io/v3/<api>");
-
-    // Deploy contract
-    const deploy = async() => {
-
-        console.log('Attempting to deploy from account:', address);
-        const incrementer = new web3.eth.Contract(abi);
-
-        const incrementerTx = incrementer.deploy({
-            data: bytecode
-            // arguments: [],
-        })
-        const createTransaction = await web3.eth.accounts.signTransaction({
-                from: address,
-                data: incrementerTx.encodeABI(),
-                gas: 3000000,
-            },
-            privKey
-        )
-        const createReceipt = web3.eth.sendSignedTransaction(createTransaction.rawTransaction).then((res) => {
-            console.log('Contract deployed at address', res.contractAddress);
-        });
-
-    };
-    deploy();
+    return res.status(200).json({result});
 }
