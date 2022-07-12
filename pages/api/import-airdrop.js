641665823383bb1d17761b2e0dbc25866ffd35ed
0xiliyan <0xiliyan@membernft.io>
2022-07-12 15:55:14 +0300

    Import NFT airdrops page done

--- pages/api/import-airdrop.js
+++ pages/api/import-airdrop.js
@@ -1,14 +1,43 @@
 import fs from "fs";
 import path from "path";
 import {connection} from "../../helpers/db";
-import config from "../../config";
-import csv from "fast-csv";
+import * as csv from "fast-csv";
+import { createRouter } from "next-connect";
+import multer from 'multer';
+import {ethers} from "ethers";
 
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        return importAirdrop(req, res);
-    }
-}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: './storage/airdrops',
+        filename: (req, file, cb) => cb(null, 'import.csv'),
+    }),
+});
+const uploadMiddleware = upload.single('airdrop');
+
+const apiRoute = createRouter();
+apiRoute.use(uploadMiddleware).post(async (req, res) => {
+    const processed = await importAirdrop(req, res);
+
+    return res.json({processed});
+});
+
+// create a handler from router with custom
+// onError and onNoMatch
+export default apiRoute.handler({
+    onError: (err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).end("Something broke!");
+    },
+    onNoMatch: (req, res) => {
+        res.status(404).end("Page is not found");
+    },
+});
+
+export const config = {
+    api: {
+        bodyParser: false, // Disallow body parsing, consume as stream
+    },
+};
 
 /**
  * Import mint jobs from a CSV file with the following format: wallet, tokens_allowed
@@ -18,19 +47,23 @@ export default function handler(req, res) {
  * @returns {Promise<void>}
  */
 const importAirdrop = async (req, res) => {
-    const projectId = req.query.collection_id;
-    const airdrops = await readCsv(path.resolve('storage/airdrop.csv'));
+    const collectionId = req.body.collection_id;
+    const airdrops = await readCsv(path.resolve('storage/airdrops/import.csv'));
 
     airdrops.forEach(airdrop => {
-        connection.query('INSERT INTO mints SET project_id = ?, tx_hash = ?, wallet = ?, tokens_allowed = ?',
-            [projectId, 'airdrop', airdrop[0], airdrop[1]], (error, results, fields) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log(`Wallet ${airdrop[0]} added to airdrop with tokens to mint: ${airdrop[1]}`);
-                }
-            });
+        if (ethers.utils.isAddress(airdrop[0]) && !isNaN(airdrop[1])) {
+            connection.query('INSERT INTO mints SET collection_id = ?, tx_hash = ?, wallet = ?, tokens_allowed = ?',
+                [collectionId, 'airdrop', airdrop[0], airdrop[1]], (error, results, fields) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log(`Wallet ${airdrop[0]} added to airdrop with tokens to mint: ${airdrop[1]}`);
+                    }
+                });
+        }
     });
+
+    return airdrops.length;
 }
 
 const readCsv = (filepath) => {
@@ -45,7 +78,7 @@ const readCsv = (filepath) => {
         })
         .on('end', rowCount => {
             resolve(airdrops);
-            console.log(`Parsed ${rowCount} rows`)
+            console.log(`Parsed ${rowCount} rows`);
         });
     });
 }
--- pages/collections/[id]/airdrop.js
+++ pages/collections/[id]/airdrop.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Heading, FormControl, FormLabel, Input, FormHelperText, Select, Radio, RadioGroup, Button } from '@chakra-ui/react';
 import {Box, Stack, Container, Flex, Text} from "@chakra-ui/layout";
 import styled, { css } from 'styled-components';
 import axios from "axios";
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {Img} from "@chakra-ui/image";
 
 const Airdrop = () => {
     const router = useRouter();
+    const fileUpload = useRef(null);
 
     const [collection, setCollection] = useState({});
     const [isLoading, setIsLoading] = useState(false);
+    const [processedRows, setProcessedRows] = useState();
 
     useEffect(() => {
         if (router.query.id) {
@@ -23,19 +26,54 @@ const Airdrop = () => {
         setCollection(data);
     }
 
+    const onChangeHandler = async (event) => {
+        if (!event.target.files?.length) {
+            return;
+        }
+
+        setIsLoading(true);
+
+        const formData = new FormData();
+        formData.append('airdrop', event.target.files[0]);
+        formData.append('collection_id', collection.id);
+
+        const config = {
+            headers: { 'content-type': 'multipart/form-data' },
+            onUploadProgress: (event) => {
+                console.log(`Current progress:`, Math.round((event.loaded * 100) / event.total));
+            },
+        };
+
+        const {data} = await axios.post('/api/import-airdrop', formData, config);
+
+        setProcessedRows(data.processed);
+        setIsLoading(false);
+
+        // reset file upload field
+        fileUpload.current.value = '';
+    };
+
     return (
         <>
-            <Heading as="h3" size="lg" mb={15}>Import CSV list of wallets for NFT Airdrop</Heading>
-            <p><b>Step 1</b>: Import CSV sheet with two columns - first column is wallet address, second column is number of NFTs to mint for the wallet (e.g. 3)</p>
+            <Heading as="h3" size="lg" mb={15}>{collection.name} - Import CSV list of wallets for NFT Airdrop</Heading>
+            <p><b>Step 1</b>: Import CSV sheet with two columns - first column is wallet address, second column is number of NFTs to mint for the wallet (e.g. 3). No need for column headers.</p>
             <p><b>Step 2</b>: Click import button below to parse the CSV list and import into database.</p>
-            <p><b>Step 3</b>: Mint NFTs from the collection mint page.</p>
+            <p><b>Step 3</b>: Mint NFTs from the collection's mint page <Link href={`/collections/${collection.id}/mint`}><b style={{cursor: "pointer"}}>here</b></Link>.</p>
+
+            <Box my={25}>
+                <Box mb={5}>Example CSV sheet screenshot below:</Box>
+                <Img src="/img/import-airdrop-example.png" />
+            </Box>
             <Box mt="25" width="500px">
-                <Input type="file" border="0" padding="0" />
-                <Link href="/collections/create">
-                    <Button colorScheme="blue" mt={15}>
+                <form>
+                    <Input type="file" name="airdrop" ref={fileUpload} accept=".csv" border="0" padding="0" onChange={onChangeHandler} hidden  />
+                    <Button colorScheme="blue" onClick={() => fileUpload.current.click()} isLoading={isLoading}>
                         Import CSV
                     </Button>
-                </Link>
+                    {processedRows &&
+                    <Box mt={5}><b>Imported rows for airdrop: {`${processedRows}`}</b></Box>
+                    }
+                </form>
             </Box>
         </>
     )
